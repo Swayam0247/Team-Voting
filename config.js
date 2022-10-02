bae005d78595a929658c10c7d8d63b132a49ec39
Swayam47 <72565749+Swayam0247@users.noreply.github.com>
2022-10-02 19:56:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBfhrIzCXgnJIM5y_y-lygcjzAGCXUcTNw",
+  authDomain: "team-voting-19f57.firebaseapp.com",
+  projectId: "team-voting-19f57",
+  storageBucket: "team-voting-19f57.appspot.com",
+  messagingSenderId: "746122972131",
+  appId: "1:746122972131:web:7462c0de1b817c24a0aed6"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
